a716de2feca1cfaf7a8fbbba35aa1c81563d0467
Babis Babalis <babisbabalis@gmail.com>
2021-01-14 20:47:47 +0200

    documentation added for algorithm.

--- src/min_path_ops/min_path_operations.py
+++ src/min_path_ops/min_path_operations.py
@@ -111,7 +111,49 @@ def get_dijkstra_matching_df(df, dijkstra_nodes_list, id='osmid'):
 
 def k_best(dijkstra_edges, mandatory_nodes_list, median_node,
            end_node, results_list):
-    """ doc here IMP
+    """ Solves a minimum path by following certain nodes as given.
+    
+    Algorithm
+    ---------
+    k_best(mandatory_list, median, end, results_list):
+        if mandatory_list is empty:
+            return
+        if median == end:
+            end = mandatory_list.pop_last_item()
+            median = mandatory_list.pop_last_item()
+            results_list.append(dijkstra(median, end))
+            return k_best(mandatory_list, median, end, results_list)
+        end = median
+        median = mandatory_list.pop()
+        results_list.append(dijkstra(median, end))
+        return k_best(mandatory_list, median, end, results_list)
+    
+    Explanation
+    -----------
+    It is a recursive routine/algorithm. It has end condition,
+    start condition and recursive part.
+    
+    END CONDITION: list of mandatory nodes is empty. Exits.
+    START CONDITION: median == end. Special nodes for starting the algo.
+    run a dijkstra with (end-1, end) nodes and get the result.
+    RECURSION:
+        - start from the end of the path.
+        - move nodes by -1 position in list. So:
+            * end = median
+            * median = median - 1
+        - re-run a dijkstra and
+        - return a k-best.
+    
+    Args:
+        dijkstra_edges (list): edges that consist a dijkstra path.
+        mandatory_nodes_list (list): list of all mandatory nodes k-best
+                                    has to visit.
+        median_node (int): a node between origin and destination in path.
+        end_node (int): the ending node for a dijkstra path.
+        results_list (list): list of dijkstra runs.
+
+    Returns:
+        Nothing is being returned actually. results_list is being updated.
     """
     #pdb.set_trace()
     if not mandatory_nodes_list:
@@ -132,7 +174,19 @@ def k_best(dijkstra_edges, mandatory_nodes_list, median_node,
 
 
 def refine_k_best_results(results_list):
-    """ doc
+    """Method to refine the results of a k-best algorithm.
+    
+    Goal of this method is to take a recursive tuples list and 
+    extract the tuple elements one by one in a new list. In addition,
+    it computes and returns the total distance of the local min-paths
+    that have run.
+
+    Args:
+        results_list (list): A list which contains a tuple of tuples
+        in a recursive way.
+
+    Returns:
+        float, list: total distance, list with k_best paths
     """
     total_distance = 0
     k_best_nodes_list = []
