487941e82ece13196a284922a27a3825d7f72ec5
Babis Babalis <babisbabalis@gmail.com>
2021-04-13 18:22:11 +0300

    use case for flora. fast prototyping

---
+++ src/flora.py
@@ -0,0 +1,17 @@
+""" Module to fast prototype the merge of the network modeled by networkx tools from OSM and custom input.
+"""
+
+import pandas as pd
+import network_operations as net_ops
+import pdb
+
+
+def main():
+    # read input (file with custom data and network)
+    # match custom input to network
+    # add traffic to edges that connect nodes of custom input
+    pass
+
+
+if __name__ == '__main__':
+    main()
