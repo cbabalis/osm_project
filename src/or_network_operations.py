c02a7306adc8b0c13b9543e0c5c84422336ec160
Babis Babalis <babisbabalis@gmail.com>
2021-04-18 22:51:58 +0300

    or net ops created

---
+++ src/or_network_operations.py
@@ -0,0 +1,26 @@
+"""Vehicles Routing Problem (VRP) by google.
+
+https://developers.google.com/optimization/routing/vrp"""
+
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+
+
+def print_solution(data, manager, routing, solution):
+    """Prints solution on console."""
+    max_route_distance = 0
+    for vehicle_id in range(data['num_vehicles']):
+        index = routing.Start(vehicle_id)
+        plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
+        route_distance = 0
+        while not routing.IsEnd(index):
+            plan_output += ' {} -> '.format(manager.IndexToNode(index))
+            previous_index = index
+            index = solution.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(
+                previous_index, index, vehicle_id)
+        plan_output += '{}\n'.format(manager.IndexToNode(index))
+        plan_output += 'Distance of the route: {}m\n'.format(route_distance)
+        print(plan_output)
+        max_route_distance = max(route_distance, max_route_distance)
+    print('Maximum of the route distances: {}m'.format(max_route_distance))
